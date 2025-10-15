270879efd3b42aaae82a260455b6d85856b65cfc
Kendell R <KTibow@users.noreply.github.com>
2025-10-15 13:21:59 -0700

    sign in stuff

--- src/settings/providers/_GitHubWarning.svelte
+++ src/settings/providers/_GitHubWarning.svelte
@@ -1,8 +1,15 @@
 <p>
-  ⚠️ To continue, you must <a href="https://github.com/signup">have a GitHub account</a> with
-  Copilot (try
-  <a href="https://github.com/settings/education/benefits">enabling education benefits</a>
-  and <a href="https://github.com/github-copilot/free_signup">enabling Copilot</a>).
+  ⚠️ To continue, you must have a GitHub account (<a
+    href="https://github.com/login"
+    >sign in</a
+  >
+  /
+  <a href="https://github.com/signup">sign up</a>) with Copilot (try
+  <a href="https://github.com/settings/education/benefits"
+    >enabling education benefits</a
+  >
+  and
+  <a href="https://github.com/github-copilot/free_signup">enabling Copilot</a>).
 </p>
 
 <style>
